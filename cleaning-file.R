9b6104f346b85031377369475cda9eb1600b1711
Rebecca Green <rgreen@path.org>
2024-02-22 17:05:09 -0800

    misc data cleaning (replace unknowns/declined and remove cg interview variables)

--- cleaning-file.R
+++ cleaning-file.R
@@ -42,6 +42,18 @@ df <- subset(df, cg_consent_yn==1) # remove non-consented, N = 1
 df <- subset(df, is.na(exclude_data_rsn) | exclude_data_rsn!=3) # remove records w/insufficient data, N = 42
 
 # Replace all 999 with NA
+df$cg_edu_mother <- ifelse(df$cg_edu_mother>=99, NA, df$cg_edu_mother)
+df$care_referral <- ifelse(df$care_referral>=99, NA, df$care_referral)
+df$nearest_facility_study <- ifelse(df$nearest_facility_study>=99, NA, df$nearest_facility_study)
+df$household_head <- ifelse(df$household_head>=99, NA, df$household_head)
+df$household_toilet <- ifelse(df$household_toilet>=99, NA, df$household_toilet)
+df$shared_toilet_yn <- ifelse(df$shared_toilet_yn>=99, NA, df$shared_toilet_yn)
+df$household_fuel <- ifelse(df$household_fuel>=99, NA, df$household_fuel)
+df$household_water_time <- ifelse(df$household_water_time>=99, NA, df$household_water_time)
+df$household_floor <- ifelse(df$household_floor>=99, NA, df$household_floor)
+df$travel_time <- ifelse(df$travel_time>=99, NA, df$travel_time)
+df$travel_cost <- ifelse(df$travel_cost>=99, NA, df$travel_cost)
+
 df$spo2 <- ifelse(df$spo2>=999, NA, df$spo2)
 df$pr <- ifelse(df$pr>=999, NA, df$pr)
 df$rr <- ifelse(df$rr>=999, NA, df$rr)
@@ -50,6 +62,7 @@ df$weight <- ifelse(df$weight>=999, NA, df$weight)
 df$height <- ifelse(df$height>=999, NA, df$height)
 df$muac <- ifelse(df$muac>=999, NA, df$muac)
 df$hb <- ifelse(df$hb>=999, NA, df$hb)
+df$refferal <- ifelse(df$refferal>=99, NA, df$refferal)
 
 # Calculate age
 df$bdate <- paste(df$birth_year, df$birth_month, "15", sep = "-")
@@ -98,7 +111,7 @@ df$country <- case_when(df$facility_name=="TZN01"|df$facility_name=="TZN02" ~ "T
                         df$facility_name=="KYA01"|df$facility_name=="KYA02" ~ "Kenya")
 df$bmi <- (df$weight/(df$height^2))*10000
 df$danger_assessed_yn <- ifelse(!is.na(df$drink_yn) & !is.na(df$vomit_yn) & !is.na(df$convulsions_yn), 1, 0) # all danger signs assessed by provider
-df$mainsxs_assessed_yn <- ifelse(!is.na(df$cough_yn) & !is.na(df$dyspnea_days) & !is.na(df$diarrhea_yn) & !is.na(df$fever_yn) & !is.na(df$earproblem_yn) & !is.na(df$anemia_yn), 1, 0) # all amins sxs assessed by provider
+df$mainsxs_assessed_yn <- ifelse(!is.na(df$cough_yn) & !is.na(df$dyspnea_days) & !is.na(df$diarrhea_yn) & !is.na(df$fever_yn) & !is.na(df$earproblem_yn) & !is.na(df$anemia_yn), 1, 0) # all main sxs assessed by provider
 
 # Calculate z-scores for nutrition metrics; use to create indicator variables -- not possible, sex of child not collected during study
 
@@ -108,6 +121,9 @@ df$anemia_status <- case_when(df$hb<7.0 ~ "severe",
                               df$hb<11 & df$hb>=10.0 ~ "mild",
                               df$hb>=11.0 ~ "none")
 df$anemia_status <- ifelse(df$age_months<6, NA, df$anemia_status)
+
+# Remove caregiver interview variables
+df <- df %>% select(-c("cg_interview_yn", "cg_sex", "cg_overall_comfort", "cg_like_most", "cg_like_least", "cg_prov_challenges_yn", "cg_prov_challenges", "cg_overall_satisfied", "cg_confident_use", "cg_confident_performance", "cg_adequate_assess_yn", "cg_adequate_assess_rsn", "cg_advantage", "cg_concerns", "cg_compare_assess", "cg_compare_assess_rsn", "cg_useful", "cg_useful_rsn", "cg_rec_device", "cg_rec_facility", "cg_rec_facility_rsn", "cg_overall_impression", "cg_time_change_yn", "cg_time_change", "cg_understand_purpose", "cg_dx_confidence", "cg_discomfort", "cg_discomfort_des", "cg_recommend", "cg_change_desire", "cg_othe_comments", "caregiver_interview_complete"))
 
 # Write file as .csv to shared Box folder
 write.csv(df, "C:/Users/rgreen/Box/3_Output 3/Hybrid study/Implementation Study Analysis/implementation-data_clean_2024-02-15.csv")
